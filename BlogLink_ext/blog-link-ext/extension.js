7434a8a63c0af5424aee2f7106fda798a3b5f826
akifEco <jahin@echologyx.com>
2024-09-05 20:20:55 +0600

    Unknown commit

--- BlogLink_ext/blog-link-ext/extension.js
+++ BlogLink_ext/blog-link-ext/extension.js
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
+
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const axios = require('axios');
+const convert = require('xml-js');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,23 +11,59 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+	console.log('Congratulations, your extension "blog-link-ext" is now active!');
+	const res = await axios.get('https://blog.webdevsimplified.com/rss.xml')
+	const jsonResult = JSON.parse(convert.xml2json(res.data, {
+		compact: true,
+		spaces: 2
+	}));
+	const items = jsonResult.rss.channel.item.map
+	(item => {
+		return {
+			label: item.title._text,
+			detail: item.description._text,
+			link: item.link._text
+		}
+	});
+
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "blog-link-ext" is now active!');
+	
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('blog-link-ext.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	try{
+		const disposable = vscode.commands.registerCommand('blog-link-ext.searchBlogExample', 
+			async function () {
+				const item = await vscode.window.showQuickPick(items,{
+				matchOnDetail: true,
+			})
+			if (item == null) return
+			
+			vscode.env.openExternal(item.link);
+			}
+		);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Blog Link Extension!');
-	});
+		context.subscriptions.push(disposable);
+	}
+	catch(e){
+		vscode.window.showErrorMessage('Error: ' + e);
+	}
 
-	context.subscriptions.push(disposable);
+	let disposable2 = vscode.commands.registerCommand('blog-link-ext.test', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World 2 from blog-link-ext.test!');
+    });
+
+    context.subscriptions.push(disposable2);
+	
+	
+
+	
 }
 
 // This method is called when your extension is deactivated
